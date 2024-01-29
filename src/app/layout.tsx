34cefdf251188f07b37322e0c2f34968b9ebc625
unknown <yjs12180825@gmail.com>
2024-01-29 23:38:04 +0900

    :sparkles: 프로젝트 시작 :sparkles:

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "설레임",
+  description: "학교에서 느끼는 당신만의 설레임",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
